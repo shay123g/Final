a06283320e269d2ce206ea5c0609adc5a4cf5e16
Shay <gazit.shay@gmail.com>
2019-07-08 17:57:24 +0300

    +

--- Infrastructure/Utilities/CommonOps.java
+++ Infrastructure/Utilities/CommonOps.java
@@ -1,6 +1,64 @@
 package Utilities;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
 /*
 This class contain general functions that will be used across all the project
  */
-public class CommonOps extends Base {
+public class CommonOps extends Base
+{
+    public static String getData (String nodeName) throws ParserConfigurationException, SAXException, IOException
+    {
+        File fXmlFile = new File("./ConfigFiles/DataConfig.xml");
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.parse(fXmlFile);
+        doc.getDocumentElement().normalize();
+        return doc.getElementsByTagName(nodeName).item(0).getTextContent();
+    }
+
+    public static void InitBrowser(String BrowserType) throws ParserConfigurationException, SAXException, IOException
+    {
+        switch (BrowserType.toLowerCase())
+        {
+            case ("chrome"):
+            {
+                driver= InitChromeDriver();
+                break;
+            }
+            case ("firefox"):
+            {
+                driver= InitFFDriver();
+                break;
+            }
+        }
+        driver.manage().window().maximize();
+        driver.get(getData("url"));
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    public static WebDriver InitChromeDriver() throws IOException, SAXException, ParserConfigurationException
+    {
+        System.setProperty("webdriver.chrome.driver",getData("chromedriverpath"));
+        WebDriver local_driver=new ChromeDriver();
+        return local_driver;
+    }
+
+    public static WebDriver InitFFDriver() throws IOException, SAXException, ParserConfigurationException
+    {
+        System.setProperty("webdriver.gecko.driver",getData("firefoxdriverpath"));
+        WebDriver local_driver=new FirefoxDriver();
+        return local_driver;
+    }
 }
--- TestCases/Tests/wikipedia.java
+++ TestCases/Tests/wikipedia.java
@@ -8,21 +8,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class wikipedia extends CommonOps
 {
     @BeforeClass
-    public void StartSession()
-    {
-        System.setProperty("webdriver.chrome.driver","./Drivers/chromedriver.exe");
-        driver=new ChromeDriver();
+    public void StartSession() throws IOException, SAXException, ParserConfigurationException {
+        InitBrowser(getData("BrowserType"));
         ManagePages.Init();
-        driver.manage().window().maximize();
-        driver.get("https://www.wikipedia.org/");
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
     }
 
     @AfterClass
